756192e95b98367c9f1b1da7d50c81c73b23675b
Guilherme Oliveira <guilhergoa@hotmail.com>
2020-09-18 20:11:26 -0300

    feat: add more datas in index user

--- api/app/Controllers/Http/UsersController.ts
+++ api/app/Controllers/Http/UsersController.ts
@@ -3,7 +3,7 @@ import User from '../../Models/User'
 export default class UsersController {
   public async index () {
     const users = await User.query()
-      .select(['id','email', 'created_at','updated_at'])
+      .select(['id','email','name', 'nick', 'avatar', 'bio', 'created_at','updated_at'])
 
     return users
   }
