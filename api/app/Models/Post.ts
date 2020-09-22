56d4a08cc33920bd3daaeb53d47590bf882b9cac
Matheus <matheusfelipeferreiramartins@gmail.com>
2020-09-22 11:48:17 -0300

    Community and Post BE

---
+++ api/app/Models/Post.ts
@@ -0,0 +1,44 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+
+import User from 'App/Models/User'
+import Community from 'App/Models/Community'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true, columnName: 'id' })
+  public id: number
+
+  @column()
+  public user_id: number
+
+  @column()
+  public community_id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public content: string
+
+  @column()
+  public likes: number
+
+  @column()
+  public comments: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @belongsTo(() => User, {
+    foreignKey: 'user_id',
+  })
+  public user: BelongsTo<typeof User>
+
+  @belongsTo(() => Community, {
+    foreignKey: 'community_id',
+  })
+  public community: BelongsTo<typeof Community>
+}
---
+++ api/database/migrations/1600778519066_posts.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Posts extends BaseSchema {
+  protected tableName = 'posts'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('community_id').notNullable().references('id').inTable('communities').onDelete('CASCADE')
+      table.string('title', 255).notNullable()
+      table.string('content').notNullable()
+      table.integer('likes').defaultTo(0)
+      table.integer('comments').defaultTo(0)
+      table.timestamps(true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
