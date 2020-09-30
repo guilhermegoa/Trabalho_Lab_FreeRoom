3d646a7a5162234a32ea2304a64a09cee1f5e75a
Guilherme Oliveira <guilhergoa@hotmail.com>
2020-09-29 23:55:35 -0300

    feat: created componet  free route

---
+++ client/src/components/FreeRoute/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+const FreeRoute = ({
+  path, exact, component, isLogged,
+}) => {
+  return (
+    <>
+      {!isLogged
+      ? <Route path={path} exact={exact} component={component} />
+      : <Redirect to="/main" />
+      }
+    </>
+  );
+};
+
+const mapStateToProps = ({ user }) => ({
+  isLogged: user.isLogged,
+  isValidToken: user.isValidToken,
+});
+
+export default connect(mapStateToProps, null)(FreeRoute);
